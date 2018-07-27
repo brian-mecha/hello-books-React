d513183d0e7c0123f55a03ceb71c648b544edf6d
brian-mecha <brianmecha@gmail.com>
2018-07-27 12:47:03 +0300

    removes jest

--- src/tests/state-functions.test.js
+++ src/tests/state-functions.test.js
@@ -1,10 +1,4 @@
-// describe("Addition", () => {
-//   it("knows that 2 and 2 make 4", () => {
-//     expect(2 + 2).toBe(4);
-//   });
-// });
-
-// test('two plus two is four', () => {
-//   expect(2 + 2).toBe(4);
-// });
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
+});
 
