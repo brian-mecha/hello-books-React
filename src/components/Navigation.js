ae9de0023782db004df689a6b439e98d68bbe606
brian-mecha <brianmecha@gmail.com>
2018-08-06 17:09:46 +0300

    Adds a custom 404 page for Hello Books

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
-import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { Switch, Link, BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./Login";
 import Home from "./Home";
 import BorrowingHistory from "./BorrowingHistory";
@@ -11,12 +11,36 @@ import ViewBook from "./templates/ViewBook";
 import { PrivateRoute } from "../utils/auth";
 
 class Navigation extends React.Component {
-  
   render() {
+    const NoMatch = ({ location }) => (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="error-template">
+              <br />
+              <br />
+              <br />
+              <h1>Oops!</h1>
+              <h2>Page Not Found</h2>
+              <div className="error-details">
+                The requested page for <code>{location.pathname}</code> was not
+                found!
+              </div>
+              <div className="error-actions">
+                <Link to="/" className="btn btn-info btn-lg">
+                <i className="fa fa-home" /> Back to Homepage
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+    
     return (
       <div>
         <Router>
-          <Fragment>
+          <Switch>
             <Route exact path="/" component={Home} />
             <PrivateRoute path="/profile" component={Profile} />
             <Route path="/login" component={Login} />
@@ -25,7 +49,8 @@ class Navigation extends React.Component {
             <PrivateRoute path="/book/add" component={AddBook} />
             <PrivateRoute path="/book/edit/:id" component={EditBook} />
             <Route path="/book/view/:id" component={ViewBook} />
-          </Fragment>
+            <Route component={NoMatch} />
+          </Switch>
         </Router>
       </div>
     );
